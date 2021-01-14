f88c294fbac48ec9381671fe1fdd4e613f16a76a
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2021-01-14 19:02:38 +0100

    Problem mixed discretization

---
+++ arch_opt_exp/problems/discretization.py
@@ -0,0 +1,98 @@
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright: (c) 2020, Deutsches Zentrum fuer Luft- und Raumfahrt e.V.
+Contact: jasper.bussemaker@dlr.de
+"""
+
+import numpy as np
+from typing import *
+from pymoo.model.repair import Repair
+from pymoo.model.population import Population
+from pymoo.model.problem import MetaProblem, Problem
+
+__all__ = ['MixedIntProblem', 'MixedIntRepair']
+
+
+class MixedIntProblem(MetaProblem):
+    """Creates a mixed-integer problem from an existing problem, by mapping the first n (if not given: all) variables to
+    integers, with a given number of choices."""
+    problem: Problem
+
+    def __init__(self, problem: Problem, n_choices=11, n_vars_mixed_int: int = None):
+        super(MixedIntProblem, self).__init__(problem)
+
+        self.n_choices = n_choices
+
+        if n_vars_mixed_int is None:
+            n_vars_mixed_int = self.problem.n_var
+        self.n_vars_mixed_int = n_vars_mixed_int
+
+        if not self.problem.has_bounds():
+            raise ValueError('Underlying problem should have bounds defined')
+        self.xl_orig = self.problem.xl
+        self.xu_orig = self.problem.xu
+
+        self.xl = xl = np.copy(self.problem.xl)
+        xl[:self.n_vars_mixed_int] = 0
+        self.xu = xu = np.copy(self.problem.xu)
+        xu[:self.n_vars_mixed_int] = self.n_choices-1
+
+        n_vars_real = self.problem.n_var-self.n_vars_mixed_int
+        self.mask = ['int' for _ in range(self.n_vars_mixed_int)]+['real' for _ in range(n_vars_real)]
+        self.is_int_mask = [self.mask[i] == 'int' for i in range(len(self.mask))]
+
+    def get_repair(self) -> Repair:
+        return MixedIntRepair(self.is_int_mask)
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        x_underlying = self._map_x(self._correct_x(x))
+        self.problem._evaluate(x_underlying, out, *args, **kwargs)
+
+    def _map_x(self, x: np.ndarray) -> np.ndarray:
+        x = np.copy(x)
+
+        n = self.n_vars_mixed_int
+        xl, xu = self.xl, self.xu
+        xl_orig, xu_orig = self.xl_orig, self.xu_orig
+
+        x[:, :n] = ((x[:, :n]-xl[:n])/(xu[:n]-xl[:n]))*(xu_orig[:n]-xl_orig[:n])+xl_orig[:n]
+        return x
+
+    def _correct_x(self, x: np.ndarray) -> np.ndarray:
+        x = np.copy(x)
+
+        n = self.n_vars_mixed_int
+        x[:, :n] = np.round(x[:, :n].astype(np.float64)).astype(np.int)
+        return x
+
+
+class MixedIntRepair(Repair):
+    """Repair operator to make sure that integer variables are actually integers after sampling or mating."""
+
+    def __init__(self, is_int_mask):
+        super(MixedIntRepair, self).__init__()
+
+        self.is_int_mask = is_int_mask
+
+    def _do(self, problem: Problem, pop: Union[Population, np.ndarray], **kwargs):
+        is_array = not isinstance(pop, Population)
+        x = pop if is_array else pop.get("X")
+
+        is_int_mask = self.is_int_mask
+        x[:, is_int_mask] = np.round(x[:, is_int_mask].astype(np.float64)).astype(np.int)
+
+        if is_array:
+            return x
+        pop.set("X", x)
+        return pop
---
+++ tests/test_problems.py
@@ -0,0 +1,101 @@
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright: (c) 2020, Deutsches Zentrum fuer Luft- und Raumfahrt e.V.
+Contact: jasper.bussemaker@dlr.de
+"""
+
+import pytest
+from pymoo.model.problem import Problem
+from pymoo.algorithms.nsga2 import NSGA2
+from pymoo.model.algorithm import Algorithm
+from pymoo.factory import get_problem, get_sampling, get_crossover, get_mutation
+from pymoo.operators.mixed_variable_operator import MixedVariableSampling, MixedVariableMutation, MixedVariableCrossover
+
+from arch_opt_exp.experimenter import *
+from arch_opt_exp.metrics.performance import *
+from arch_opt_exp.problems.discretization import *
+
+
+@pytest.fixture
+def problem() -> Problem:
+    return get_problem('zdt1')
+
+
+@pytest.fixture
+def algorithm() -> Algorithm:
+    return NSGA2(pop_size=100)
+
+
+def test_mixed_int_problem(problem, algorithm):
+    mixed_int_problem = MixedIntProblem(problem, n_choices=11, n_vars_mixed_int=15)
+
+    hv_metric = DeltaHVMetric(mixed_int_problem.pareto_front())
+    exp = Experimenter(mixed_int_problem, algorithm, n_eval_max=2000, metrics=[hv_metric])
+    result = exp.run_effectiveness(repeat_idx=0, seed=0)
+
+    hv_values = result.metrics[hv_metric.name].results()['delta_hv']
+    assert hv_values[-1] < hv_values[0]
+    # result.metrics[hv_metric.name].plot()
+
+
+def test_mixed_int_problem_repair(problem):
+    mixed_int_problem = MixedIntProblem(problem, n_choices=11, n_vars_mixed_int=15)
+
+    algorithm = NSGA2(
+        pop_size=100,
+        repair=mixed_int_problem.get_repair(),
+    )
+
+    hv_metric = DeltaHVMetric(mixed_int_problem.pareto_front())
+    exp = Experimenter(mixed_int_problem, algorithm, n_eval_max=2000, metrics=[hv_metric])
+    result = exp.run_effectiveness(repeat_idx=0, seed=0)
+
+    hv_values = result.metrics[hv_metric.name].results()['delta_hv']
+    assert hv_values[-1] < hv_values[0]
+    # result.metrics[hv_metric.name].plot()
+
+
+def test_mixed_int_problem_masked(problem):
+    mixed_int_problem = MixedIntProblem(problem, n_choices=11, n_vars_mixed_int=15)
+
+    sampling = MixedVariableSampling(mixed_int_problem.mask, {
+        'real': get_sampling('real_random'),
+        'int': get_sampling('int_random'),
+    })
+
+    crossover = MixedVariableCrossover(mixed_int_problem.mask, {
+        'real': get_crossover('real_sbx', prob=1., eta=3.),
+        'int': get_crossover('int_sbx', prob=1., eta=3.),
+    })
+
+    mutation = MixedVariableMutation(mixed_int_problem.mask, {
+        'real': get_mutation('real_pm', eta=3.),
+        'int': get_mutation('int_pm', eta=3.),
+    })
+
+    algorithm = NSGA2(
+        pop_size=100,
+        sampling=sampling,
+        crossover=crossover,
+        mutation=mutation,
+        repair=mixed_int_problem.get_repair(),
+    )
+
+    hv_metric = DeltaHVMetric(mixed_int_problem.pareto_front())
+    exp = Experimenter(mixed_int_problem, algorithm, n_eval_max=2000, metrics=[hv_metric])
+    result = exp.run_effectiveness(repeat_idx=0, seed=0)
+
+    hv_values = result.metrics[hv_metric.name].results()['delta_hv']
+    assert hv_values[-1] < hv_values[0]
+    # result.metrics[hv_metric.name].plot()
