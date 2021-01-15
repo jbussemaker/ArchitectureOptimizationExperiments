ab0d491d055740e8cbbb69873c134ba86a133abe
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2021-01-15 16:08:02 +0100

    Added Multiple Dominance Relations support

---
+++ arch_opt_exp/algorithms/mdr.py
@@ -0,0 +1,196 @@
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright: (c) 2020, Deutsches Zentrum fuer Luft- und Raumfahrt e.V.
+Contact: jasper.bussemaker@dlr.de
+"""
+
+import contextlib
+import numpy as np
+from typing import *
+from enum import Enum
+
+from pymoo.algorithms.nsga2 import NSGA2
+from pymoo.util.dominator import Dominator
+
+__all__ = ['MetricType', 'DominanceRelation', 'MultipleDominanceRelations', 'inject_dominator', 'NSGA2MultiDR']
+
+
+class MetricType(Enum):
+    F = 1  # Objective
+    G = 2  # Constraint
+    CV = 3  # Constraint violation (max constraint violation for all constraints)
+
+
+DominanceRelation = Tuple[Tuple[MetricType, int], ...]
+
+
+class MultipleDominanceRelations(Dominator):
+    """
+    Dominator that uses the Multiple Dominance Relations (MDR) principle. MDR allows to apply different dominance
+    relations to different metrics, both taking objectives and constraints into account. A hierarchy of constraints and
+    objectives is determined, whereby lower-hierarchy metrics are searched for within the Pareto-optimal set of metrics
+    that are higher up in the hierarchy. It is shown that the best designs are found for the following order: design
+    constraints, primary objectives, lower priority objectives. This approach is especially interesting for
+    multi-objective design problems where there exists a hierarchy in design goals, for example coming from different
+    goals of stakeholders.
+
+    References:
+    Cook, L.W., "Design Optimization Using Multiple Dominance Relations", 2020, 10.1002/nme.6316
+    Phillips, S., "Assessing the Significance of Nesting Order in Optimization Using Multiple Dominance Relations",
+        2020, 10.2514/6.2020-3134
+    """
+
+    def __init__(self, hierarchy: List[DominanceRelation]):
+        self.hierarchy = hierarchy
+
+        self._dom = Dominator.get_relation
+
+    def get_relation(self, a, b, cva=None, cvb=None, ga=None, gb=None) -> int:
+        if ga is not None and len(ga) == 0:
+            ga = None
+        if gb is not None and len(gb) == 0:
+            gb = None
+
+        def _get_ab(dom_rel: DominanceRelation) -> Tuple[np.ndarray, np.ndarray]:
+            a_ = np.zeros((len(dom_rel),))
+            b_ = np.zeros((len(dom_rel),))
+
+            for i, (met_type, idx) in enumerate(dom_rel):
+                if met_type == MetricType.F:
+                    a_[i] = a[idx]
+                    b_[i] = b[idx]
+
+                elif met_type == MetricType.G:
+                    if ga is not None:
+                        a_[i] = np.max([ga[idx], 0.])
+                    if gb is not None:
+                        b_[i] = np.max([gb[idx], 0.])
+
+                elif met_type == MetricType.CV:
+                    if cva is not None:
+                        a_[i] = cva
+                    if cvb is not None:
+                        b_[i] = cvb
+
+            return a_, b_
+
+        # Loop over dominance relations
+        for dom_relation in self.hierarchy:
+            rel = self._dom(*_get_ab(dom_relation))
+
+            # If this relation is not indifferent, we return as we don't need to check any further relations
+            if rel != 0:
+                return rel
+
+        # None of the relations returned a specific result
+        return 0
+
+    def calc_domination_matrix_loop(self, F, G):
+        n = F.shape[0]
+        CV = np.sum(G * (G > 0).astype(np.float), axis=1)
+        M = np.zeros((n, n))
+        for i in range(n):
+            for j in range(i + 1, n):
+                M[i, j] = self.get_relation(F[i, :], F[j, :], CV[i], CV[j], G[i, :], G[j, :])
+                M[j, i] = -M[i, j]
+
+        return M
+
+    def calc_domination_matrix(self, F, _F=None, epsilon=0.0):
+        return self.calc_domination_matrix_loop(F, np.zeros((F.shape[0], 0)))
+
+
+@contextlib.contextmanager
+def inject_dominator(dominator: Union[Dominator, Type[Dominator]]):
+    """
+    Context manager for injecting a specific Dominator subclass (or instance thereof) to be used globally in pymoo.
+    Unfortunately this is a necessity as pymoo does not expose a nicely OOP interface for using the Dominator in other
+    places of the code.
+    """
+
+    funcs = ['get_relation', 'calc_domination_matrix_loop', 'calc_domination_matrix']
+
+    orig_funcs = [getattr(Dominator, func) for func in funcs]
+    for func in funcs:
+        setattr(Dominator, func, getattr(dominator, func))
+
+    yield
+
+    for i, func in enumerate(funcs):
+        setattr(Dominator, func, orig_funcs[i])
+
+
+class NSGA2MultiDR(NSGA2):
+    """NSGA2 with Multiple Dominance Relations instead of the regular dominance relation."""
+
+    def __init__(self, multi_dr: MultipleDominanceRelations, **kwargs):
+        super(NSGA2MultiDR, self).__init__(**kwargs)
+
+        self.multi_dr = multi_dr
+
+    def _initialize(self):
+        with inject_dominator(self.multi_dr):
+            super(NSGA2MultiDR, self)._initialize()
+
+    def _next(self):
+        with inject_dominator(self.multi_dr):
+            super(NSGA2MultiDR, self)._next()
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from arch_opt_exp.experimenter import *
+    from pymoo.problems.many.dtlz import DTLZ2
+    from arch_opt_exp.metrics.filters import *
+    from arch_opt_exp.metrics.convergence import *
+    from arch_opt_exp.metrics.performance import *
+    from pymoo.factory import get_reference_directions
+
+    with Experimenter.temp_results():
+        # Define algorithms to run
+        multi_dr_ = MultipleDominanceRelations([
+            ((MetricType.F, 0), (MetricType.F, 1)),
+            ((MetricType.F, 2), (MetricType.F, 3)),
+        ])
+
+        algorithms = [
+            NSGA2MultiDR(multi_dr_, pop_size=100),
+            NSGA2(pop_size=100),
+        ]
+
+        # Define problem and metrics
+        problem = DTLZ2(n_var=13, n_obj=4)
+        ref_dirs = get_reference_directions('das-dennis', 4, n_partitions=12)
+        pf = problem.pareto_front(ref_dirs)
+        metrics = [
+            # Metrics for evaluating the algorithm performance
+            DeltaHVMetric(pf),
+            IGDMetric(pf),
+
+            # Metrics for detecting convergence
+            ExpMovingAverageFilter(ConsolidationRatioMetric(), n=5),
+            ExpMovingAverageFilter(MutualDominationRateMetric(), n=5),
+        ]
+        plot_names = [['delta_hv'], None, ['cr'], ['mdr']]
+
+        # Run algorithms
+        results = [Experimenter(problem, algorithm, n_eval_max=10000, metrics=metrics).run_effectiveness()
+                   for algorithm in algorithms]
+
+        # Plot metrics
+        for ii, metric in enumerate(metrics):
+            ExperimenterResult.plot_compare_metrics(
+                results, metric.name, titles=[algo.__class__.__name__ for algo in algorithms],
+                plot_value_names=plot_names[ii], plot_evaluations=True, show=False)
+        plt.show()
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -16,10 +16,15 @@ Contact: jasper.bussemaker@dlr.de
 """
 
 import pytest
-from pymoo.factory import get_problem
+import numpy as np
 from pymoo.model.problem import Problem
+from pymoo.algorithms.nsga2 import NSGA2
+from pymoo.util.dominator import Dominator
+from pymoo.problems.many.dtlz import DTLZ2
+from pymoo.factory import get_problem, get_reference_directions
 
 from arch_opt_exp.experimenter import *
+from arch_opt_exp.algorithms.mdr import *
 from arch_opt_exp.metrics.filters import *
 from arch_opt_exp.metrics.performance import *
 from arch_opt_exp.metrics.convergence import *
@@ -75,3 +80,115 @@ def test_simulated_annealing(problem):
 
     igd = res.metrics[igd_metric.name].results()['indicator']
     assert igd[0] != 0
+
+
+def test_mdr():
+    mdr = MultipleDominanceRelations([
+        ((MetricType.G, 0), (MetricType.G, 1)),  # First check dominance among the constraints
+        ((MetricType.F, 0), (MetricType.F, 1)),  # Then among the first two objectives
+        ((MetricType.F, 2), (MetricType.F, 3)),  # THen among the last two objectives
+    ])
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 0, 0]), b=np.array([0, 0, 0, 0]), ga=np.array([0, 0]), gb=np.array([0, 0])) == 0
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 0, 0]), b=np.array([0, 0, 0, 0]), ga=np.array([-1, 0]), gb=np.array([0, -1])) == 0
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 0, 0]), b=np.array([0, 0, 0, 0]), ga=np.array([1, 0]), gb=np.array([0, 0])) == -1
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 0, 0]), b=np.array([0, 0, 0, 0]), ga=np.array([1, 0]), gb=np.array([0, 1])) == 0
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 0, 0]), b=np.array([0, 0, 0, 0]), ga=np.array([1, 0]), gb=np.array([2, 1])) == 1
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 0, 0]), b=np.array([1, 1, 0, 0]), ga=np.array([0, 0]), gb=np.array([0, 0])) == 1
+
+    assert mdr.get_relation(
+        a=np.array([1, 1, 0, 0]), b=np.array([0, 0, 0, 0]), ga=np.array([0, 0]), gb=np.array([0, 0])) == -1
+
+    assert mdr.get_relation(
+        a=np.array([1, 0, 0, 0]), b=np.array([0, 1, 0, 0]), ga=np.array([0, 0]), gb=np.array([0, 0])) == 0
+
+    assert mdr.get_relation(
+        a=np.array([1, 0, 0, 0]), b=np.array([0, 1, 1, 1]), ga=np.array([0, 0]), gb=np.array([0, 0])) == 1
+
+    assert mdr.get_relation(
+        a=np.array([1, 0, 1, 1]), b=np.array([0, 1, 0, 0]), ga=np.array([0, 0]), gb=np.array([0, 0])) == -1
+
+    assert mdr.get_relation(
+        a=np.array([1, 0, 1, 0]), b=np.array([0, 1, 0, 1]), ga=np.array([0, 0]), gb=np.array([0, 0])) == 0
+
+    assert mdr.get_relation(
+        a=np.array([0, 0, 1, 1]), b=np.array([1, 1, 0, 0]), ga=np.array([0, 0]), gb=np.array([0, 0])) == 1
+
+    # Regular behavior: first check the constraint violation, then the objectives
+    mdr_reg = MultipleDominanceRelations([
+        ((MetricType.CV, 0),),
+        ((MetricType.F, 0), (MetricType.F, 1), (MetricType.F, 2), (MetricType.F, 3)),
+    ])
+
+    for _ in range(10):
+        f = np.random.random((10, 4))
+        g = np.random.random((10, 2))-.5
+
+        m_nds = Dominator.calc_domination_matrix_loop(f, g)
+        m_mdr_reg = mdr_reg.calc_domination_matrix_loop(f, g)
+        assert np.all(m_nds == m_mdr_reg)
+
+        m_mdr = mdr.calc_domination_matrix_loop(f, g)
+        assert not np.all(m_nds == m_mdr)
+
+    # Check the direct matrix methos
+    mdr = MultipleDominanceRelations([((MetricType.F, 0), (MetricType.F, 1))])
+    f = np.random.random((20, 2))
+    m_nds = Dominator.calc_domination_matrix(f)
+    m_mdr = mdr.calc_domination_matrix(f)
+    assert np.all(m_nds == m_mdr)
+
+
+def test_inject_dominator():
+    mdr = MultipleDominanceRelations([
+        ((MetricType.G, 0), (MetricType.G, 1)),
+        ((MetricType.F, 0), (MetricType.F, 1)),
+        ((MetricType.F, 2), (MetricType.F, 3)),
+    ])
+
+    f = np.random.random((20, 4))
+    g = np.random.random((20, 2)) - .5
+    m_nds = Dominator.calc_domination_matrix_loop(f, g)
+
+    with inject_dominator(mdr):
+        m_mdr = Dominator.calc_domination_matrix_loop(f, g)
+
+    assert not np.all(m_nds == m_mdr)
+
+
+def test_nsga2_mdr():
+    problem = DTLZ2(n_var=13, n_obj=4)
+    ref_dirs = get_reference_directions('das-dennis', 4, n_partitions=12)
+    metric = DeltaHVMetric(problem.pareto_front(ref_dirs))
+
+    mdr = MultipleDominanceRelations([
+        ((MetricType.F, 0), (MetricType.F, 1)),
+        ((MetricType.F, 2), (MetricType.F, 3)),
+    ])
+
+    nsga2 = NSGA2(pop_size=50)
+    exp = Experimenter(problem, nsga2, n_eval_max=1000, metrics=[metric])
+    result = exp.run_effectiveness(repeat_idx=0, seed=0)
+
+    nsga2_mdr = NSGA2MultiDR(mdr, pop_size=50)
+    exp_mdr = Experimenter(problem, nsga2_mdr, n_eval_max=1000, metrics=[metric])
+    result_mdr = exp_mdr.run_effectiveness(repeat_idx=0, seed=0)
+
+    values = result.metrics[metric.name].values['delta_hv']
+    values_mdr = result_mdr.metrics[metric.name].values['delta_hv']
+    assert values[0] == values_mdr[0]
+    assert values[-1] != values_mdr[-1]
+
+    ExperimenterResult.plot_compare_metrics(
+        [result, result_mdr], metric.name, titles=['MDR', 'ND'], plot_value_names=['delta_hv'], show=False)
