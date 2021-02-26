ed577581afaf859ccf5310b236d544411a335948
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2021-02-26 14:43:08 +0100

    Added copyright headers

--- arch_opt_exp/surrogates/smt/smt_surrogate_model.py
+++ arch_opt_exp/surrogates/smt/smt_surrogate_model.py
@@ -1,3 +1,20 @@
+"""
+Licensed under the GNU General Public License, Version 3.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.gnu.org/licenses/gpl-3.0.html.en
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright: (c) 2021, Deutsches Zentrum fuer Luft- und Raumfahrt e.V.
+Contact: jasper.bussemaker@dlr.de
+"""
+
 import numpy as np
 from typing import *
 from arch_opt_exp.surrogates import SurrogateModel
