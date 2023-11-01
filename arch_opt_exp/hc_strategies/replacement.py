e47681f92d18ef38178a7c1f0e03c7d2982bb519
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2023-11-01 18:06:06 +0100

    Use MD GP in replacement HC strategy

--- arch_opt_exp/hc_strategies/replacement.py
+++ arch_opt_exp/hc_strategies/replacement.py
@@ -15,8 +15,11 @@ Copyright: (c) 2023, Deutsches Zentrum fuer Luft- und Raumfahrt e.V.
 Contact: jasper.bussemaker@dlr.de
 """
 import numpy as np
+from typing import Optional
 from scipy.spatial import distance
 from smt.surrogate_models.krg import KRG
+from sb_arch_opt.problem import ArchOptProblemBase
+from sb_arch_opt.algo.arch_sbo.models import *
 from sb_arch_opt.algo.arch_sbo.hc_strategy import *
 
 __all__ = ['ReplacementHCStrategyBase', 'GlobalWorstReplacement', 'LocalReplacement', 'PredictedWorstReplacement']
@@ -62,9 +65,15 @@ class LocalReplacement(ReplacementHCStrategyBase):
 class PredictedWorstReplacement(ReplacementHCStrategyBase):
     """Replace failed values by the worst predicted value of a model trained on the valid points"""
 
-    def __init__(self, mul: float = 1.):
+    def __init__(self, mul: float = 1., kpls_n_dim: Optional[int] = 10, ignore_hierarchy=True):
+        self._problem = None
         self.mul = mul
+        self._kpls_n_dim = kpls_n_dim
+        self._ignore_hierarchy = ignore_hierarchy
         super().__init__()
+
+    def initialize(self, problem: ArchOptProblemBase):
+        self._problem = problem
 
     def _replace_y(self, x_failed: np.ndarray, y_failed: np.ndarray, x_valid: np.ndarray, y_valid: np.ndarray) \
             -> np.ndarray:
@@ -74,16 +83,19 @@ class PredictedWorstReplacement(ReplacementHCStrategyBase):
         norm[norm < 1e-6] = 1e-6
         y_norm = (y_valid - y_min) / norm
 
-        # Train Kriging surrogate model
-        x_norm = self._normalization.forward
+        # Train MD Kriging surrogate model
+        kwargs = {}
+        if self._kpls_n_dim is not None and x_valid.shape[1] > self._kpls_n_dim:
+            kwargs['kpls_n_comp'] = self._kpls_n_dim
 
-        model = KRG(print_global=False)
-        model.set_training_values(x_norm(x_valid), y_norm)
+        model, normalization = ModelFactory(self._problem).get_md_kriging_model(
+            corr='abs_exp', theta0=[1e-2], ignore_hierarchy=self._ignore_hierarchy, **kwargs)
+        model.set_training_values(normalization.forward(x_valid), y_norm)
         model.train()
 
         # Predict values of failed points
-        y_predict = model.predict_values(x_norm(x_failed))
-        y_predict_var = model.predict_variances(x_norm(x_failed))
+        y_predict = model.predict_values(normalization.forward(x_failed))
+        y_predict_var = model.predict_variances(normalization.forward(x_failed))
 
         # Replace failed points with mean + sigma*var of the prediction
         y_replace = y_predict + self.mul * np.sqrt(y_predict_var)
